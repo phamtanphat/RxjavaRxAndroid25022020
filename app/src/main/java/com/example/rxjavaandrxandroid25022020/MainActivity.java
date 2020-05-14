1c2eb04bef08e9b1ec3b8221d8f3a61b0ae198f4
phatdroid94 <phatdroid94@gmail.com>
2020-05-14 21:00:02 +0700

    init observable

--- app/src/main/java/com/example/rxjavaandrxandroid25022020/MainActivity.java
+++ app/src/main/java/com/example/rxjavaandrxandroid25022020/MainActivity.java
@@ -3,6 +3,16 @@ package com.example.rxjavaandrxandroid25022020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Buoc 1 : Tao ra 1 observable
+        Observable<String> observableString =
+                Observable.just("A","B","C","D","E","F");
+        // Buoc 2 : Lang nghe du lieu
+        observableString
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull String s) {
+                        Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+
+
+
     }
 }
