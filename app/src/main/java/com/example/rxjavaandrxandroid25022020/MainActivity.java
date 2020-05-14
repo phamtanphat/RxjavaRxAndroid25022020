8097fdd04956d33be620da257a0ed7d9787dd9af
phatdroid94 <phatdroid94@gmail.com>
2020-05-14 21:10:10 +0700

    finish

--- app/src/main/java/com/example/rxjavaandrxandroid25022020/MainActivity.java
+++ app/src/main/java/com/example/rxjavaandrxandroid25022020/MainActivity.java
@@ -12,6 +12,7 @@ import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Function;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,20 +23,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Buoc 1 : Tao ra 1 observable
         Observable<String> observableString =
-                Observable.just("A","B","C","D","E","F");
+                Observable.just("1","2","3","4","5","6");
         // Buoc 2 : Lang nghe du lieu
         observableString
                 .subscribeOn(Schedulers.io())
+                .map(new Function<String, Integer>() {
+                    @Override
+                    public Integer apply(String s) throws Throwable {
+                        Toast.makeText(MainActivity.this, s + "", Toast.LENGTH_SHORT).show();
+                        return Integer.parseInt(s);
+                    }
+                })
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<String>() {
+                .subscribe(new Observer<Integer>() {
                     @Override
                     public void onSubscribe(@NonNull Disposable d) {
 
                     }
 
                     @Override
-                    public void onNext(@NonNull String s) {
-                        Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+                    public void onNext(@NonNull Integer integer) {
+//                        Toast.makeText(MainActivity.this, integer +"", Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
